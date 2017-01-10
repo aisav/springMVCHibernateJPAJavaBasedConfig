5c76c1c90d96923cba54eb012ba84ae79f448e30
aisav <artur.isaverdyan@gmail.com>
2017-01-10 21:43:48 +0400

    hibernate configuration

---
+++ src/main/java/art/test/config/DbConfig.java
@@ -0,0 +1,81 @@
+package art.test.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+import java.util.Properties;
+
+/**
+ * Created by art on 10.01.2017.
+ */
+//@EnableJpaRepositories(basePackages = {"art.test.dao"})
+//@ComponentScan(basePackages = {"art.test.dao"})
+@EnableTransactionManagement
+@Configuration
+public class DbConfig {
+
+    @Autowired
+    private Environment environment;
+
+    @Bean
+    public DataSource datasource(){
+
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(environment.getProperty("mysql.driver"));
+        dataSource.setUrl(environment.getProperty("mysql.url"));
+        dataSource.setUsername(environment.getProperty("mysql.username"));
+        dataSource.setPassword(environment.getProperty("mysql.password"));
+        return dataSource;
+    }
+
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter() {
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        vendorAdapter.setDatabase(Database.MYSQL);
+        vendorAdapter.setShowSql(true);
+        return  vendorAdapter;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
+        factoryBean.setJpaVendorAdapter(jpaVendorAdapter());
+        factoryBean.setDataSource(datasource());
+        factoryBean.setPackagesToScan("art.test.domain");
+        Properties jpaProperties = new Properties();
+        jpaProperties.setProperty("hibernate.hbm2ddl.auto","create-drop");
+        factoryBean.setJpaProperties(jpaProperties);
+        return factoryBean;
+    }
+
+/*    @Bean
+    public BlogPostService blogPostService(){
+        return new BlogPostServiceImpl();
+    }
+
+    @Bean
+    public UserService userService(){
+        return new UserServiceImpl();
+    }
+
+    @Bean
+    public JpaTransactionManager transactionManager(EntityManagerFactory entityManagerFactory){
+
+        JpaTransactionManager jpaTransactionManager = new JpaTransactionManager();
+        jpaTransactionManager.setEntityManagerFactory(entityManagerFactory);
+        return  jpaTransactionManager;
+    }*/
+}
+
